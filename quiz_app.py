8bbcda3374d81a2a9dab333705d0245a19105bdd
Ajard <100705569+ajald24@users.noreply.github.com>
2025-01-31 10:48:06 +0900

    quiz_app.py

--- quiz_app.py
+++ quiz_app.py
@@ -43,7 +43,7 @@ def export_questions_to_csv():
     query = 'SELECT * FROM  questions'
     df = pd.read_sql_query(query, conn)
     # データをCSVファイルにエクスポート
-    csv = df.to_csv(index=False,encoding='cp932')
+    csv = df.to_csv(index=False,encoding='shift-jis')
     # CSVファイルをダウンロード可能にする
     st.download_button(
         label="CSVでダウンロード",
